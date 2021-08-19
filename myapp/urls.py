8fbe4306896a85d3d87c7c05070c611263a3fdfa
jignesh <jigneshpatel.ec.er@gmail.com>
2021-08-19 19:16:44 +0530

    filter Ui button added

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('getorder',views.getorder,name='getorder'),
     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render,redirect
 from .models import Customer,Product,Order
+from django.http.response import Http404, HttpResponse, JsonResponse
+
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
+
 
 def index(request):
     c=Customer.objects.all()
-    params={'c':c}
-    return render (request,'index1.html',params)
+    o=Order.objects.all()
+    params={'c':c,'o':o}
+    return render (request,'index.html',params)
+
+@csrf_exempt
+def getorder(request):
+    if request.method=="POST":
+        customers=Customer.objects.all(id=request.POST['state_id'])
+        orders=Order.objects.filter(state_id=customers)
 
 
 
+
+
+
+'''@csrf_exempt
+def getorder1(request):
+    if request.method=="POST":
+        state=Customer.objects.get(id=request.POST['state_id'])
+        orders=Order.objects.filter(state_id=state.id)
+        alldetails=[]
+        for i in orders:
+            alldetails.append({'name':i.fname,'email':i.email,'mobile':i.mobile,'orderdate':i.order_date,'order_number':i.order_number})
+            return JsonResponse({'alldetails':alldetails})
+    else:
+        return redirect('/')    '''       
+
+            
